5e379873cf535d46b3e6d4dbe827b31bc07a0644
chaechae <juold0568@naver.com>
2025-06-02 17:45:08 +0900

    feat: like 엔티티 필드 누락 추가

--- src/main/java/io/chcch/starfinder/domain/like/entity/Like.java
+++ src/main/java/io/chcch/starfinder/domain/like/entity/Like.java
@@ -1,9 +1,14 @@
 package io.chcch.starfinder.domain.like.entity;
 
+import io.chcch.starfinder.domain.post.entity.Post;
+import io.chcch.starfinder.domain.user.entity.User;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,7 +18,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @Builder
-@Table(name = "likes")
+@Table(name = "post_likes")
 @NoArgsConstructor
 @AllArgsConstructor
 public class Like {
@@ -21,5 +26,13 @@ public class Like {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
 
