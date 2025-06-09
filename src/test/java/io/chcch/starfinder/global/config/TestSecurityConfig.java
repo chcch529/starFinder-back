93a731c721e29b8e89dd7403667060858057e604
chaechae <juold0568@naver.com>
2025-06-09 09:44:21 +0900

    test: ci 프로파일 추가

--- src/test/java/io/chcch/starfinder/global/config/TestSecurityConfig.java
+++ src/test/java/io/chcch/starfinder/global/config/TestSecurityConfig.java
@@ -2,17 +2,34 @@ package io.chcch.starfinder.global.config;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
-@TestConfiguration
+@Configuration
+@Profile({"test", "ci"})
 public class TestSecurityConfig {
 
     @Bean
     public SecurityFilterChain testSecurityFilterChain(HttpSecurity http) throws Exception {
         return http
             .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()) // 모든 요청 허용
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
             .build();
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(); // OAuth 끔
+    }
+
+    @Bean
+    public OAuth2AuthorizedClientService authorizedClientService() {
+        return new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository());
     }
 }
